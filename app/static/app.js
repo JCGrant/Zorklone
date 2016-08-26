1669291d9d6e6f6edf98a2878cb86afdfddf0803
JCGrant <jamescolin.grant@gmail.com>
2016-08-26 14:18:35 +0100

    Refactor client side into javascript only app.

--- app/static/app.js
+++ app/static/app.js
@@ -2,16 +2,23 @@
 
 const ENTER_KEY_CODE = 13;
 
-var terminal = document.getElementById('terminal');
+var story = document.createElement('div');
 
-var input = document.getElementById('input');
-input
+var terminal = document.createElement('pre');
+
+var userInput = document.createElement('input');
+userInput
   .addEventListener('keyup', function(event) {
-  event.preventDefault();
-  if (event.keyCode == ENTER_KEY_CODE) {
-    terminal.innerText += input.value + '\n';
-    input.value = '';
-  }
-});
+    event.preventDefault();
+    if (event.keyCode == ENTER_KEY_CODE) {
+      terminal.innerText += userInput.value + '\n';
+      userInput.value = '';
+    }
+  });
+
+var appElement = document.getElementById('app');
+appElement.appendChild(story);
+appElement.appendChild(terminal);
+appElement.appendChild(userInput);
 
 })();
